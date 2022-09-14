a3fcb5dd5e57ade357341b05907ad2b87c939e86
nicole-drml <doromal.cole@gmail.com>
2022-09-14 17:08:37 +0800

    made the game work with winner / draw. used prettier

--- index.js
+++ index.js
@@ -1,69 +1,125 @@
 const cells = document.querySelectorAll("[data-cell]");
-const newGameBtn = document.querySelector("[data-new-game]");
+const boardDisplay = document.querySelector("[data-board]");
 
+const xStartBtn = document.querySelector("[data-x-button]");
+const oStartBtn = document.querySelector("[data-o-button]");
+
+const newGameBtn = document.querySelector("[data-new-game]");
 const activePlayer = document.querySelector("[data-active-player]");
 
-const xClass = 'x'
-const oClass = 'o'
-let xTurn
+const endgameMsg = document.querySelector("[data-endgame-message]");
 
-
-cells.forEach((cell) => {
-    cell.addEventListener("click", makeMove, {once: true})
-}) 
-
-
-//(board[0][1]).push('hello')
-//board[0][1] = 'X'
-// board[0][0]
-// board[0][1]
-// let third = board[0][2]
-
+const xClass = "x";
+const oClass = "o";
+let oTurn = "";
+let currentTurn = "";
+let activeTurn = "";
 
 const board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
-console.log(cells)
-console.log(board)
-// for (const cell of cells) {
-//     cell.addEventListener("mouseover", hoverMark)
-// }
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-// function  hoverMark(cell, currentTurn) {
-//     currentTurn = xTurn ? xClass : oClass
-//     cell.classList.add(currentTurn)
-// }
+xStartBtn.addEventListener("click", () => {
+  boardDisplay.classList.remove("o-turn");
+  activePlayer.textContent = xClass;
+  boardDisplay.classList.add("x-turn");
+  oTurn = false;
+});
 
+oStartBtn.addEventListener("click", () => {
+  boardDisplay.classList.remove("x-turn");
+  activePlayer.textContent = oClass;
+  boardDisplay.classList.add("o-turn");
+  oTurn = true;
+});
 
-function makeMove (e) {
-    const cell = e.target
-    const currentTurn = xTurn ? xClass : oClass
-    const activeTurn = xTurn ? oClass: xClass 
-    activePlayer.textContent = activeTurn
-    placeMark(cell, currentTurn)
-    swapTurns()
-    // // toArray()
-    // console.log(cell)
+cells.forEach((cell) => {
+  cell.addEventListener("click", makeMove, { once: true });
+});
+
+function makeMove(e) {
+  const cell = e.target;
+  activeTurn = oTurn ? xClass : oClass;
+  currentTurn = oTurn ? oClass : xClass;
+  activePlayer.textContent = activeTurn;
+  placeMark(cell, currentTurn);
+  if (checkWin(currentTurn)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  }
+  swapTurns(activeTurn);
+  disableStartBtns();
+  console.log(board);
 }
 
-
-function  placeMark(cell, currentTurn) {
-    cell.classList.add(currentTurn)
-    cell.classList.remove('o-turn')
-    cell.classList.remove('x-turn')
+function placeMark(cell, currentTurn) {
+  cell.classList.add(currentTurn);
+  boardDisplay.classList.remove(`${currentTurn}-turn`);
 }
 
-function swapTurns() {
-    xTurn = !xTurn
+function checkWin(currentTurn) {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cells[index].classList.contains(currentTurn);
+    });
+  });
 }
+
+function swapTurns(activeTurn) {
+  oTurn = !oTurn;
+  boardDisplay.classList.add(`${activeTurn}-turn`);
+}
+
+function disableStartBtns() {
+  oStartBtn.setAttribute("disabled", "disabled");
+  xStartBtn.setAttribute("disabled", "disabled");
+}
+
+function endGame(draw) {
+  if (draw) {
+    endgameMsg.textContent = `It's a draw!`;
+  } else {
+    endgameMsg.textContent = `${currentTurn} wins!`;
+  }
+}
+
+function isDraw() {
+  return [...cells].every((cell) => {
+    return cell.classList.contains(xClass) || cell.classList.contains(oClass);
+  });
+}
+
+console.log(cells);
+console.log([...cells]); //NodeList to Array
+// console.log(board);
+// console.log(board[2][0])
+
+// if (boardDisplay.classList.contains('o-turn')) {
+//     currentTurn = oClass;
+//     activeTurn = xClass;
+//    } else {
+//     currentTurn = xClass;
+//     activeTurn = oClass;
+//    }
 
 let k = 0;
 for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++){
-        board[i][j]=cells[k];
-        k++;
-    }
+  for (let j = 0; j < board[i].length; j++) {
+    board[i][j] = cells[k];
+    k++;
+  }
 }
