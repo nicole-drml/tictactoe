a323798b73bcb8d9a887584bb93ab417aa149661
nicole-drml <doromal.cole@gmail.com>
2022-09-20 16:13:48 +0800

    removed media queries and fixed codes in index.js

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ let boardHistory = [
   ],
 ];
 
-
 const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -71,42 +70,18 @@ backwardBtn.addEventListener("click", backward);
 
 let b = 0;
 let f = 0;
-
-// x = b - f;
 function backward() {
   board = boardHistory[boardHistory.length - b];
   let availablePrevious = boardHistory.length - b;
-  console.log("avail" + availablePrevious);
 
-  console.log("b: " + b);
-  console.log("f: " + f);
   if (availablePrevious > 0) {
-    cells.forEach((cell) => {
-      cell.classList.remove(playerO);
-      cell.classList.remove(playerX);
-    });
-    let r = 0;
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        cells[r].textContent = board[i][j];
-        if (cells[r].textContent.includes(playerX)) {
-          cells[r].classList.add(playerX);
-        } else if (cells[r].textContent.includes(playerO)) {
-          cells[r].classList.add(playerO);
-        }
-        r++;
-      }
-    }
+    displayHistoryBoard();
     b++;
     f--;
   } else {
-    backwardBtn.classList.add("blocked");
-    backwardBtn.style.opacity = ".5";
-    backwardBtn.disabled = true;
+    disableBtn(backwardBtn);
   }
-  forwardBtn.classList.remove("blocked");
-  forwardBtn.style.opacity = "1";
-  forwardBtn.disabled = false;
+  enableBtn(forwardBtn);
 }
 
 forwardBtn.addEventListener("click", forward);
@@ -115,56 +90,63 @@ function forward() {
   f = 1;
   f++;
   board = boardHistory[boardHistory.length - b + f];
-  let availableForward = boardHistory.length - b + f;
-  console.log("avail" + availableForward);
-
-  console.log("b: " + b);
-  console.log("f: " + f);
   if (b > f) {
-    cells.forEach((cell) => {
-      cell.classList.remove(playerO);
-      cell.classList.remove(playerX);
-    });
-    let r = 0;
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        cells[r].textContent = board[i][j];
-        if (cells[r].textContent.includes(playerX)) {
-          cells[r].classList.add(playerX);
-        } else if (cells[r].textContent.includes(playerO)) {
-          cells[r].classList.add(playerO);
-        }
-        r++;
-      }
-    }
+    displayHistoryBoard();
     b--;
   } else {
-    forwardBtn.classList.add("blocked");
-    forwardBtn.style.opacity = ".5";
-    forwardBtn.disabled = true;
+    disableBtn(forwardBtn);
   }
-  backwardBtn.classList.remove("blocked");
-  backwardBtn.style.opacity = "1";
-  backwardBtn.disabled = false;
+  enableBtn(backwardBtn);
+}
+
+function disableBtn(button) {
+  button.classList.add("blocked");
+  button.style.opacity = ".5";
+  button.disabled = true;
+}
+
+function enableBtn(button) {
+  button.classList.remove("blocked");
+  button.style.opacity = "1";
+  button.disabled = false;
+}
+
+function displayHistoryBoard() {
+  cells.forEach((cell) => {
+    cell.classList.remove(playerO);
+    cell.classList.remove(playerX);
+  });
+  let r = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      cells[r].textContent = board[i][j];
+      if (cells[r].textContent.includes(playerX)) {
+        cells[r].classList.add(playerX);
+      } else if (cells[r].textContent.includes(playerO)) {
+        cells[r].classList.add(playerO);
+      }
+      r++;
+    }
+  }
 }
 
 xStartBtn.addEventListener("click", () => {
-  currentTurn = "x";
   playerOTurn = false;
-  boardDisplay.classList.add("x-turn");
-  whoseTurnSpan.textContent = playerX;
-  askPlayerDiv.style.display = "none";
+  beginGame(playerX);
   setupBoard();
 });
 
 oStartBtn.addEventListener("click", () => {
-  currentTurn = "o";
   playerOTurn = true;
-  boardDisplay.classList.add("o-turn");
-  whoseTurnSpan.textContent = playerO;
-  askPlayerDiv.style.display = "none";
+  beginGame(playerO);
   setupBoard();
 });
+
+function beginGame(player) {
+  currentTurn = player;
+  boardDisplay.classList.add(`${player}-turn`);
+  whoseTurnSpan.textContent = player;
+}
 
 cells.forEach((cell) => {
   cell.addEventListener("click", makeMove, { once: true });
@@ -186,22 +168,21 @@ function makeMove(e) {
     }
     swapTurns(currentTurn);
     boardHistory.push(JSON.parse(JSON.stringify(board)));
-    console.log(board);
-    console.log("board history", boardHistory);
-
-    console.log("length history", boardHistory.length);
   }
 }
 
 function placeMark(cell, currentTurn) {
-  cell.classList.add(currentTurn);
   cell.textContent = currentTurn;
-  boardDisplay.classList.remove(`${currentTurn}-turn`);
-  let k = 0;
+  let r = 0;
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      board[i][j] = cells[k].textContent;
-      k++;
+      board[i][j] = cells[r].textContent;
+      if (cells[r].textContent.includes(playerX)) {
+        cells[r].classList.add(playerX);
+      } else if (cells[r].textContent.includes(playerO)) {
+        cells[r].classList.add(playerO);
+      }
+      r++;
     }
   }
 }
@@ -215,10 +196,11 @@ function checkWin(currentTurn) {
 }
 
 function swapTurns(currentTurn) {
+  boardDisplay.classList.remove(`${currentTurn}-turn`);
   playerOTurn = !playerOTurn;
   currentTurn = playerOTurn ? playerO : playerX;
-  boardDisplay.classList.add(`${currentTurn}-turn`);
   whoseTurnSpan.textContent = currentTurn;
+  boardDisplay.classList.add(`${currentTurn}-turn`);
 }
 
 function endGame(draw) {
@@ -237,16 +219,16 @@ function endGame(draw) {
   displayScore();
 }
 
-function popupEndgame() {
-  endgameBackground.style.display = "flex";
-  boardStatus.style.visibility = "hidden";
-  whoseTurnSpan.style.visibility = "hidden";
-}
-
 function isDraw() {
   return [...cells].every((cell) => {
     return cell.textContent === playerX || cell.textContent === playerO;
   });
+}
+
+function popupEndgame() {
+  endgameBackground.style.display = "flex";
+  boardStatus.style.visibility = "hidden";
+  whoseTurnSpan.style.visibility = "hidden";
 }
 
 endgameNextGameBtn.addEventListener("click", anotherGame);
@@ -312,6 +294,7 @@ function newGamePrep() {
   board = emptyBoard;
   boardHistory = boardHistory.splice(0, 1);
   boardStatus.textContent = " ";
+  boardDisplay.classList.remove(`${currentTurn}-turn`);
 }
 
 function setupBoard() {
@@ -320,22 +303,20 @@ function setupBoard() {
   boardStatus.textContent = " playing";
   endgameBackground.style.display = "none";
   askPlayerDiv.style.display = "none";
-  boardDisplay.classList.remove(`${currentTurn}-turn`);
+  askPlayerDiv.style.display = "none";
 }
 
 function resetCounter() {
   xCounter = 0;
   oCounter = 0;
   drawCounter = 0;
-  console.log("counter");
 }
 resetCounter();
 
 function displayScore() {
-  xTotal.innerHTML = xCounter;
-  oTotal.innerHTML = oCounter;
-  drawTotal.innerHTML = drawCounter;
-  console.log("display");
+  xTotal.textContent = xCounter;
+  oTotal.textContent = oCounter;
+  drawTotal.textContent = drawCounter;
 }
 displayScore();
 
@@ -345,45 +326,3 @@ function resetPrevious() {
   backwardBtn.style.opacity = "1";
   backwardBtn.disabled = false;
 }
-// console.log(cells);
-// console.log([...cells]);
-
-// if (board.includes(playerX) || board.includes(playerO)) {
-
-// let k = 0;
-// for (let i = 0; i < board.length; i++) {
-//   for (let j = 0; j < board[i].length; j++) {
-//     board[i][j] = cells[k].textContent;
-//     k++;
-//   }
-// }
-
-// let k = 0;
-// let newBoard = [];
-// for (let i = 0; i < board.length; i++) {
-//   for (let j = 0; j < board[i].length; j++) {
-//     newBoard[i][j] = board[k];
-//     k++;
-//   }
-// }
-
-// for (let i = 0; i < board.length; i++) {
-//   for (let j = 0; j < board[i].length; j++) {
-//     if (board[i][j] === "x") {
-//       board[i][j].classList.add(playerX);
-//     } else if (board[i][j] === "o") {
-//       board[i][j].classList.add(playerO);
-//     } else {
-//       board[i][j].classList.remove(playerX)
-//       board[i][j].classList.remove(playerO)
-//     }
-//   }
-// }
-
-//   board.forEach((cell) => {
-//   if (cell === "x") {
-//     cell.classList.add(playerX);
-//   } else if (cell === "o") {
-//     cell.classList.add(playerO)
-//   }
-// })
